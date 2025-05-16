72ab7d7dd5fa3556196f45509f6b8537ba24d97e
Arne Hitzmann <arne.hitzmann@gmail.com>
2025-05-16 11:56:59 +0900

    clean-up

--- src/pc2_multi_merger_node.cpp
+++ src/pc2_multi_merger_node.cpp
@@ -18,13 +18,14 @@
 using namespace std;
 using namespace pcl;
 
-class PC2Merger : public rclcpp::Node
-{
+class PC2Merger : public rclcpp::Node {
 public:
-  PC2Merger();
+	PC2Merger();
+
 private:
-  void pc2_cb_(const sensor_msgs::msg::PointCloud2::SharedPtr& cloud, const std::string& topic);
-  void pc2_topic_parser_();
+	void pc2_cb_(const sensor_msgs::msg::PointCloud2::SharedPtr &cloud, const std::string &topic);
+
+	void pc2_topic_parser_();
 
 	std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
 	std::shared_ptr<tf2_ros::TransformListener> tfListener_;
@@ -41,8 +42,7 @@ private:
 	string pc2_topics_;
 };
 
-PC2Merger::PC2Merger() : Node("pc2_multi_merger")
-{
+PC2Merger::PC2Merger() : Node("pc2_multi_merger") {
 	this->declare_parameter<std::string>("destination_frame", "merged_cloud_frame");
 	this->declare_parameter<std::string>("cloud_destination_topic", "~/merged_cloud");
 	this->declare_parameter<std::string>("pc2_topics", "");
@@ -56,31 +56,29 @@ PC2Merger::PC2Merger() : Node("pc2_multi_merger")
 
 	this->pc2_topic_parser_();
 
-	point_cloud_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(cloud_destination_topic_, rclcpp::SensorDataQoS());
+	point_cloud_publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+		cloud_destination_topic_, rclcpp::SensorDataQoS());
 }
 
-void PC2Merger::pc2_topic_parser_()
-{
-	std::map<std::string, std::vector<std::string>> topics;
+void PC2Merger::pc2_topic_parser_() {
+	std::map<std::string, std::vector<std::string> > topics;
 
 	istringstream iss(pc2_topics_);
 	set<string> tokens;
-	copy(istream_iterator<string>(iss), istream_iterator<string>(), inserter<set<string>>(tokens, tokens.begin()));
+	copy(istream_iterator<string>(iss), istream_iterator<string>(), inserter<set<string> >(tokens, tokens.begin()));
 	std::vector<string> tmp_input_topics;
 
-	while (!tokens.empty())
-	{
+	while (!tokens.empty()) {
 		RCLCPP_INFO(this->get_logger(), "Waiting for topics ...");
 		sleep(1);
 
 		topics = this->get_topic_names_and_types();
 
-		for (const auto &topic_it : topics)
-		{
+		for (const auto &topic_it: topics) {
 			std::vector<std::string> topic_types = topic_it.second;
 
-			if (std::find(topic_types.begin(), topic_types.end(), "sensor_msgs/msg/PointCloud2") != topic_types.end() && tokens.erase(topic_it.first) > 0)
-			{
+			if (std::find(topic_types.begin(), topic_types.end(), "sensor_msgs/msg/PointCloud2") != topic_types.end() &&
+			    tokens.erase(topic_it.first) > 0) {
 				tmp_input_topics.push_back(topic_it.first);
 			}
 		}
@@ -91,75 +89,63 @@ void PC2Merger::pc2_topic_parser_()
 	tmp_input_topics.erase(last, tmp_input_topics.end());
 
 	// Do not re-subscribe if the topics are the same
-	if ((tmp_input_topics.size() != input_topics_.size()) || !equal(tmp_input_topics.begin(), tmp_input_topics.end(), input_topics_.begin()))
-	{
-    input_topics_ = tmp_input_topics;
+	if ((tmp_input_topics.size() != input_topics_.size()) || !equal(tmp_input_topics.begin(), tmp_input_topics.end(),
+	                                                                input_topics_.begin())) {
+		input_topics_ = tmp_input_topics;
 
-		if (!input_topics_.empty())
-		{
+		if (!input_topics_.empty()) {
 			cloud_subscribers_.resize(input_topics_.size());
 			clouds_modified_.resize(input_topics_.size());
 			clouds_.resize(input_topics_.size());
 			RCLCPP_INFO(this->get_logger(), "Subscribing to topics\t%ld", cloud_subscribers_.size());
-			for (std::vector<int>::size_type i = 0; i < input_topics_.size(); ++i)
-			{
+			for (std::vector<int>::size_type i = 0; i < input_topics_.size(); ++i) {
 				// workaround for std::bind https://github.com/ros2/rclcpp/issues/583
-				std::function<void(const sensor_msgs::msg::PointCloud2 ::SharedPtr)> callback =
+				std::function<void(const sensor_msgs::msg::PointCloud2::SharedPtr)> callback =
 						std::bind(
-                &PC2Merger::pc2_cb_,
-                this, std::placeholders::_1, input_topics_[i]);
-        cloud_subscribers_[i] = this->create_subscription<sensor_msgs::msg::PointCloud2>(input_topics_[i], rclcpp::SensorDataQoS(), callback);
-        clouds_modified_[i] = false;
+							&PC2Merger::pc2_cb_,
+							this, std::placeholders::_1, input_topics_[i]);
+				cloud_subscribers_[i] = this->create_subscription<sensor_msgs::msg::PointCloud2>(input_topics_[i], rclcpp::SensorDataQoS(), callback);
+				clouds_modified_[i] = false;
 				cout << input_topics_[i] << " ";
 			}
-		}
-		else
-		{
+		} else {
 			RCLCPP_INFO(this->get_logger(), "Not subscribed to any topic.");
 		}
 	}
 }
 
-void PC2Merger::pc2_cb_(const sensor_msgs::msg::PointCloud2::SharedPtr& cloud, const std::string& topic)
-{
+void PC2Merger::pc2_cb_(const sensor_msgs::msg::PointCloud2::SharedPtr &cloud, const std::string &topic) {
 	sensor_msgs::msg::PointCloud2 transformed_cloud;
-	try
-	{
-		tf_buffer_->lookupTransform(cloud->header.frame_id, destination_frame_, cloud->header.stamp, rclcpp::Duration(1, 0));
+	try {
+		tf_buffer_->lookupTransform(cloud->header.frame_id, destination_frame_, cloud->header.stamp,
+		                            rclcpp::Duration(1, 0));
 		pcl_ros::transformPointCloud(destination_frame_, *cloud, transformed_cloud, *tf_buffer_);
-	}
-	catch (tf2::TransformException &ex)
-	{
+	} catch (tf2::TransformException &ex) {
 		return;
 	}
 
-	for (std::vector<int>::size_type i = 0; i < input_topics_.size(); i++)
-	{
-		if (topic == input_topics_[i])
-		{
+	for (std::vector<int>::size_type i = 0; i < input_topics_.size(); i++) {
+		if (topic == input_topics_[i]) {
 			pcl_conversions::toPCL(transformed_cloud, clouds_[i]);
-      clouds_modified_[i] = true;
+			clouds_modified_[i] = true;
 		}
 	}
 
 	// Count how many scans we have
 	std::vector<int>::size_type totalClouds = 0;
-	for (auto && i : clouds_modified_)
-	{
+	for (auto &&i: clouds_modified_) {
 		if (i)
 			totalClouds++;
 	}
 
 	// Go ahead only if all subscribed scans have arrived
-	if (totalClouds == clouds_modified_.size())
-	{
+	if (totalClouds == clouds_modified_.size()) {
 		pcl::PCLPointCloud2 merged_cloud = clouds_[0];
-    clouds_modified_[0] = false;
+		clouds_modified_[0] = false;
 
-		for (std::vector<int>::size_type i = 1; i < clouds_modified_.size(); i++)
-		{
+		for (std::vector<int>::size_type i = 1; i < clouds_modified_.size(); i++) {
 			merged_cloud += clouds_[i];
-      clouds_modified_[i] = false;
+			clouds_modified_[i] = false;
 		}
 
 		std::shared_ptr<sensor_msgs::msg::PointCloud2> cloud_msg = std::make_shared<sensor_msgs::msg::PointCloud2>();
@@ -168,8 +154,7 @@ void PC2Merger::pc2_cb_(const sensor_msgs::msg::PointCloud2::SharedPtr& cloud, c
 	}
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 	rclcpp::init(argc, argv);
 	rclcpp::spin(std::make_shared<PC2Merger>());
 	rclcpp::shutdown();
